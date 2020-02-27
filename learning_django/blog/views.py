8c2ea301a956fb6adfae951e9f70ef3e22d2508f
Marek Polacek <mapo@onemarketing.dk>
2020-02-28 00:45:54 +0100

    Added Bootstrap

--- learning_django/blog/views.py
+++ learning_django/blog/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Marek',
+        'title': 'Blog post 1',
+        'content': 'First blog post content',
+        'date_posted': 'Feb 26, 2020'
+    },
+    {
+        'author': 'Marek',
+        'title': 'Blog post 2',
+        'content': 'Second blog post content',
+        'date_posted': 'Feb 27, 2020'
+    },
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog home</h1>')
+    context = {
+        'page': { 'title':'Blog home' },
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+    # return HttpResponse('<h1>Blog home</h1>')
 
 def about(request):
-    return HttpResponse('<h1>About this blog</h1>')
+    return render(request, 'blog/about.html', { 'page': { 'title': 'About', 'content': 'Page content'}})
+    # return HttpResponse('<h1>About this blog</h1>')
